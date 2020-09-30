01afec397c6eb3b1b9a1a88250a8fb08470282c6
Steven Topazio <sat@northpointprinting.com>
2020-09-30 14:02:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     array = programmer_hash[:dennis_ritchie][:languages]
+     array.join
 end
 
 def adding_matz
