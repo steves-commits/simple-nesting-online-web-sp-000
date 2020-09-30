d29ac32fd796da97dfda26561c59d8e095904fc4
Steven Topazio <sat@northpointprinting.com>
2020-09-30 14:08:52 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,11 +16,12 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
 	
 	programmer_hash = 
  		{
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     array = programmer_hash[:dennis_ritchie][:languages]
+     array.join
 end
 
 def adding_matz
@@ -80,6 +85,8 @@ def adding_matz
         }
      }
 
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languanges => ["LISP", "C"]}
+    programmer_hash
     
 end
 
@@ -101,7 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +129,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] << "Assembly" 
+programmer_hash
 
 end
